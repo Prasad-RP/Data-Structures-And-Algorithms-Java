e56a19ba32dfd138cc6082de1914578be4dd084b
prasad <pansareprasad3798@gmail.com>
2023-09-20 15:37:53 +0530

    working on trees

--- src/non_linear_data_structures/tress/BinaryTree.java
+++ src/non_linear_data_structures/tress/BinaryTree.java
@@ -3,7 +3,8 @@ package non_linear_data_structures.tress;
 import java.util.Scanner;
 
 /**
- * File contains all tree operations using Linked list data structure.
+ * File contains all tree operations(create , traverse) using Linked list data
+ * structure.
  * 
  * @author Prasad Pansare
  *
@@ -21,14 +22,14 @@ class Node {
 }
 
 public class BinaryTree {
+	Scanner sc = new Scanner(System.in);
 
 	/**
 	 * Recurrsive function to create a binary tree.
 	 * 
 	 * @return root Node
 	 */
-	static Node create() {
-		Scanner sc = new Scanner(System.in);
+	Node create() {
 
 		System.out.println("Enter value of Node(for exit, enter -1):");
 		int data = sc.nextInt();
@@ -42,8 +43,64 @@ public class BinaryTree {
 		return root;
 	}
 
-	public static void main(String[] args) {
+	// Tree Traversals
+	void inOrder(Node root) {
+		if (root != null) {
+			inOrder(root.leftChild);
+			System.out.println(root.data);
+			inOrder(root.rightChild);
+		}
+	}
 
-		Node root = create();
+	void preOrder(Node root) {
+		if (root != null) {
+			System.out.println(root.data);
+			preOrder(root.leftChild);
+			preOrder(root.rightChild);
+		}
+	}
+
+	void postOrder(Node root) {
+		if (root != null) {
+			postOrder(root.leftChild);
+			postOrder(root.rightChild);
+			System.out.println(root.data);
+		}
+	}
+
+	void callMenu() {
+		int ch;
+		Node root = null;
+		System.out.println("Binary Tree using Linked List");
+		do {
+			System.out.println("\n1.Create\n2.InOrder\n3.Preorder\n4.PostOrder\n5.Exit");
+			ch = sc.nextInt();
+			switch (ch) {
+			case 1:
+				root = create();
+				break;
+			case 2:
+				System.out.println("Inorder of tree..");
+				inOrder(root);
+				break;
+			case 3:
+				System.out.println("Preorder of tree..");
+				preOrder(root);
+				break;
+			case 4:
+				System.out.println("Postorder of tree..");
+				postOrder(root);
+				break;
+			case 5:
+				System.exit(0);
+			default:
+				break;
+			}
+		} while (ch != 6);
+	}
+
+	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree();
+		tree.callMenu();
 	}
 }
